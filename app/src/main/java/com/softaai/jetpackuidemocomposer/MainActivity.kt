d1e15199b15c811fb3466ed034d9168cbec254b6
amoljp19 <41947289+amoljp19@users.noreply.github.com>
2024-12-13 17:27:59 +0530

    name update

---
+++ app/src/main/java/com/softaai/jetpackuidemocomposer/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.softaai.jetpackuidemocomposer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.softaai.jetpackuidemocomposer.ui.theme.JetpackUIDemoComposerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackUIDemoComposerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Jetpack UI Demo Composer",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackUIDemoComposerTheme {
+        Greeting("Jetpack UI Demo Composer")
+    }
+}
